dcce64baa73a28a780d607e98674239c2f55fe43
KinshukK15 <97669987+KinshukK15@users.noreply.github.com>
2022-05-23 22:44:22 +0530

    Update list_intro.py

--- Sequences/list_intro.py
+++ Sequences/list_intro.py
@@ -4,9 +4,16 @@ computer_parts = ["computer",
                   "mouse",
                   "mouse pat"
                   ]
-for part in computer_parts:
-    print(part)
-print()
-print(computer_parts[2])
-print(computer_parts[0:3])
-print(computer_parts[-1])
+# for part in computer_parts:
+#     print(part)
+# print()
+# print(computer_parts[2])
+# print(computer_parts[0:3])
+# print(computer_parts[-1])
+
+
+print(computer_parts)
+
+# computer_parts[3] = "trackball"
+computer_parts[3:] = ["trackball"]
+print(computer_parts)
